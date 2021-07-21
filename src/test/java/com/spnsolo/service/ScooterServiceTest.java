ee9b87ee6c7bef057461d12adb64c150636b418a
Artem-Pukha <spnsolo270@gmail.com>
2021-07-21 12:07:05 +0300

    fixed test

--- src/test/java/com/spnsolo/service/ScooterServiceTest.java
+++ src/test/java/com/spnsolo/service/ScooterServiceTest.java
@@ -8,7 +8,6 @@ import com.spnsolo.repository.ScooterRepository;
 import com.spnsolo.service.impl.ScooterService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -18,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 public class ScooterServiceTest {
     private ScooterService service;
     private ScooterRepository repository;
